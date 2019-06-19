45b1245904346035288507a5501ee12800dc21a9
Yasutaka Kawamoto <kwmt27@gmail.com>
2019-06-19 11:26:54 +0900

    Initial Commit

---
+++ Landmarks-SwiftUI-Tutorial/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Landmarks-SwiftUI-Tutorial
+//
+//  Created by Yasutaka Kawamoto on 2019/06/19.
+//  Copyright © 2019 Yasutaka Kawamoto. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
