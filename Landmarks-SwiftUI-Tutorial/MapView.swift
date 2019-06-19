c9f3130ad65c0321b94ade90448d33ffac29b1f9
Yasutaka Kawamoto <kwmt27@gmail.com>
2019-06-19 17:41:56 +0900

    updateUIViewを実装

--- Landmarks-SwiftUI-Tutorial/MapView.swift
+++ Landmarks-SwiftUI-Tutorial/MapView.swift
@@ -13,6 +13,14 @@ struct MapView: UIViewRepresentable {
     func makeUIView(context: Context) -> MKMapView {
         MKMapView(frame: .zero)
     }
+
+    func updateUIView(_ view: MKMapView, context: UIViewRepresentableContext<MapView>) {
+        let coordinate = CLLocationCoordinate2D(
+        latitude: 34.011286, longitude: -116.166868)
+        let span = MKCoordinateSpan(latitudeDelta: 2.0, longitudeDelta: 2.0)
+        let region = MKCoordinateRegion(center: coordinate, span: span)
+        view.setRegion(region, animated: true)
+    }
 }
 
 #if DEBUG
