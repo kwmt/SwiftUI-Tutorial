e6bd05b8fbeb612e37a62fab8fab7a91f63775a0
Yasutaka Kawamoto <kwmt27@gmail.com>
2019-06-19 13:21:14 +0900

    add  CircleImage.swift

---
+++ Landmarks-SwiftUI-Tutorial/CircleImage.swift
@@ -0,0 +1,23 @@
+//
+//  CircleImage.swift
+//  Landmarks-SwiftUI-Tutorial
+//
+//  Created by Yasutaka Kawamoto on 2019/06/19.
+//  Copyright © 2019 Yasutaka Kawamoto. All rights reserved.
+//
+
+import SwiftUI
+
+struct CircleImage : View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#if DEBUG
+struct CircleImage_Previews : PreviewProvider {
+    static var previews: some View {
+        CircleImage()
+    }
+}
+#endif
