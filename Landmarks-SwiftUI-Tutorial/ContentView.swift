b91d7c7ccd4919a31a41978f3e6be4787db6748d
Yasutaka Kawamoto <kwmt27@gmail.com>
2019-06-19 12:58:35 +0900

    タイトル名変更

--- Landmarks-SwiftUI-Tutorial/ContentView.swift
+++ Landmarks-SwiftUI-Tutorial/ContentView.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct ContentView : View {
     var body: some View {
         VStack {
-            Text("Hello SwiftUI")
+            Text("Turtle Rock")
                 .font(.title)
             Text("Joshua Tree National Park")
                 .font(.subheadline)
